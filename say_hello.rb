17927e51de22b810de8b6587509302bbb71c73b4
Melissa Sperry <sperry.mel@gmail.com>
2020-02-12 05:43:50 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -2,6 +2,6 @@ def say_hello(name)
   puts "Hello #{name}!"
 end
 
-def say_hello_with_defaults (name="Ruby programmer!")
+def say_hello_with_defaults (person="Ruby programmer!")
   puts "Hello #{name}"
 end
